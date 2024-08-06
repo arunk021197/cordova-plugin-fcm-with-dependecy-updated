d77def95c027f708208eb3af4a446f7af2a59b89
arunk021197 <arunkumar.p@idp.com>
2024-08-06 16:00:27 +0530

    fcm plugin

---
+++ src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
@@ -0,0 +1,36 @@
+package com.gae.scaffolder.plugin;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.util.Log;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import com.marketo.Marketo;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Marketo marketoSdk = Marketo.getInstance(this.getApplicationContext());
+        marketoSdk.setPushNotificationToken(s);
+    }
+
+    
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+       Marketo marketoSdk = Marketo.getInstance(this.getApplicationContext());
+        marketoSdk.showPushNotification(remoteMessage);
+    }
+    // [END receive_message]
+}
